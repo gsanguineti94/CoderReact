ed1a43d9d587c75ba0af688104592c4271c6c589
Guido Sanguineti <g.sanguineti94@gmail.com>
2022-03-28 18:56:04 -0300

    Cree varios archivos en components, css, sass, etc

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,1 +1,22 @@
-React.createElement('div', {className: 'headerWrapper'})
+import './navBar.css';  
+
+function App(){
+    return(
+        <nav class="navbar">
+        <div class="navbar-center">
+          <span>
+            <i class="fas fa-bars burger"></i>
+            <ul class="nav-menu">
+              <li class="productos-btn">Productos</li>
+              <li class="negro-btn">El Negro</li>
+              <li class="nosotros-btn">Nosotros</li>
+              <li class="contacto-btn">Contacto</li>
+            </ul>
+          </span>
+          <img src="fotos\NH.png" alt="Negro Holandes Casa de Pan" />
+        </div>
+      </nav>
+    )
+}
+
+export default App;
